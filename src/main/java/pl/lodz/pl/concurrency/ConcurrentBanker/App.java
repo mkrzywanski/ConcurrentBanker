c3c8a48cad182e20b6547e2eeee675894d7c2077
michal <michak964@gmail.com>
2018-07-28 16:05:33 +0100

    Main class refactoring

--- src/main/java/pl/lodz/pl/concurrency/ConcurrentBanker/App.java
+++ src/main/java/pl/lodz/pl/concurrency/ConcurrentBanker/App.java
@@ -9,22 +9,22 @@ public class App {
 	
 	private static Logger logger = Logger.getLogger(App.class);
 	
-	private static int clientsAmount = 5;
-	private static int bankerMaxCapital = 1000;
-	private static int clientOperationLimit = 5;
-    private static int maximumRejectionsAmount = 5;
+	private static final int CLIENTS_AMOUNT = 5;
+	private static final int BANKER_MAX_CAPITAL = 1000;
+	private static final int CLIENT_OPERATION_LIMIT = 5;
+    private static final int MAXIMUM_REJECTIONS_AMOUNT = 5;
 	
     public static void main( String[] args ) throws InterruptedException {
     	logger.info("Starting program");
-    	logger.info("Clients: " + clientsAmount + " Banker's max capital: " + bankerMaxCapital + " Client's loan operation limit: " + clientOperationLimit);
+    	logger.info("Clients: " + CLIENTS_AMOUNT + " Banker's max capital: " + BANKER_MAX_CAPITAL + " Client's loan operation limit: " + CLIENT_OPERATION_LIMIT);
     	
-        Banker banker = new Banker(bankerMaxCapital, maximumRejectionsAmount);
+        Banker banker = new Banker(BANKER_MAX_CAPITAL, MAXIMUM_REJECTIONS_AMOUNT);
 
-        List<Client> clients = new ArrayList<>();
-        List<Thread> threads = new ArrayList<>();
+        List<Client> clients = new ArrayList<>(CLIENTS_AMOUNT);
+        List<Thread> threads = new ArrayList<>(CLIENTS_AMOUNT);
 
-        for(int i = 0; i < clientsAmount; i++) {
-            Client client = new Client(i, clientOperationLimit, banker);
+        for(int i = 0; i < CLIENTS_AMOUNT; i++) {
+            Client client = new Client(i, CLIENT_OPERATION_LIMIT, banker);
         	clients.add(client);
 
         	Thread clientThread = new Thread(client);
